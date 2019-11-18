5301e03d6912df3fcad9d725b3b4b829df4130ca
PietrH <48065851+PietrH@users.noreply.github.com>
2019-11-18 17:26:52 +0100

    script to convert a csv to a markdown table for GitHub

---
+++ csv_to_markdown_table.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 16:54:21 2019
+
+@author: pieterhuy
+"""
+
+import pandas as pd
+import numpy as np
+#Read the source data
+df = pd.read_csv('Abbreviations_and_Acronyms.csv')
+#append a numpy array with n times the header seperator, with n being the
+#number of columns then store this array as a pandas dataframe with setting the
+#header to the original header
+df = pd.DataFrame(
+        np.insert(df.values, 0, values=['---']*len(df.columns), axis=0),
+        columns=df.columns)
+#write this to a markdown file using the csv parser and setting the seperator
+#to pipes, we do not care about the index
+df.to_csv('Abbreviations_and_Acronyms.md', sep='|', index=False)
