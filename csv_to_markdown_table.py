f0003a97089d1fdaaeec0f8bb7cb1bb4a740b483
PietrH <48065851+PietrH@users.noreply.github.com>
2020-02-21 10:59:47 +0100

    cleaned up code

--- csv_to_markdown_table.py
+++ csv_to_markdown_table.py
@@ -14,18 +14,17 @@ df = pd.read_csv('Abbreviations_and_Acronyms.csv', quotechar='"')
 # get the domain name from the links
 ## function to format the reference link into markdown links
 def format_reference(link):
-   if not str(link)=="nan":
-       return "[reference]"+"("+str(link)+")"
+    if str(link) != "nan":
+        return "[reference]"+"("+str(link)+")"
 ## apply function
-df["Reference_Link"]=list(map(format_reference,df["Reference_Link"]))
+df["Reference_Link"] = list(map(format_reference, df["Reference_Link"]))
 # append a numpy array with n times the header seperator, with n being the
 # number of columns then store this array as a pandas dataframe with setting the
 # header to the original header
 df = pd.DataFrame(
     np.insert(df.values, 0, values=['---'] * len(df.columns), axis=0),
     columns=df.columns)
-# Replace None with nan so the markdown table will parse
-#df.fillna(value=pd.np.nan, inplace=True)
+
 # write this to a markdown file using the csv parser and setting the seperator
 # to pipes, we do not care about the index
 df.to_csv('README.md', sep='|', index=False)
