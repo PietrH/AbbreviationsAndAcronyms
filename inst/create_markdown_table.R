1e1896f0f052b18b2aac72543783eccfdd96f4d4
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-08-01 13:05:24 +0200

    remove devtools dep

--- inst/create_markdown_table.R
+++ inst/create_markdown_table.R
@@ -1,3 +1,3 @@
-devtools::install(upgrade = "never")
+install.packages(".", repos = NULL, type = "source")
 library(AbbreviationsAndAcronyms)
 csv_to_markdown_table("Abbreviations_and_Acronyms.csv")
