e36b468da2c4df8c549a6bf3446bd5ab12f0674a
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-08-01 12:13:52 +0200

    wrangle, and wrap in function

--- R/csv_to_markdown_table.R
+++ R/csv_to_markdown_table.R
@@ -1,6 +1,22 @@
-readr::read_csv("Abbreviations_and_Acronyms.csv", show_col_types = FALSE)
-
-format_link <- function(label,url) {
-  sprintf("[%s](%s)",label,url)
+format_link <- function(label, url) {
+  sprintf("[%s](%s)", label, url)
 }
 
+csv_to_markdown_table <- function(csv_path) {
+  readr::read_csv(csv_path, show_col_types = FALSE) %>%
+    dplyr::mutate(
+      Wiki_Link = dplyr::case_when(
+        is.na(Wiki_Link) ~ NA,
+        .default = format_link("Wikipedia", .data$Wiki_Link)
+      ),
+      Reference_Link = dplyr::case_when(
+        is.na(Reference_Link) ~ NA,
+        .default = format_link("Reference", .data$Reference_Link)
+      )
+    ) %>%
+    dplyr::arrange(.data$Abbreviation) %>%
+    knitr::kable() %>%
+    readr::write_lines("README.md")
+}
+
+csv_to_markdown_table("Abbreviations_and_Acronyms.csv")
